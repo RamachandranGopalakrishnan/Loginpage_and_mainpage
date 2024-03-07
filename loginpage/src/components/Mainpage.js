7ce988246d52845c20304802073ad91ee4241cd7
RamachandranGopalakrishnan <vgkanbu@gmail.com>
2024-03-07 14:54:31 +0530

    update list

--- loginpage/src/components/Mainpage.js
+++ loginpage/src/components/Mainpage.js
@@ -28,13 +28,93 @@ const Mainpage = () => {
                     <td className='px-6 py-4 whitespace-nowrap'>
                     <div className='text-sm text-gray-600'>Ramachandran</div></td>
                     <td className='px-6 py-4 whitespace-nowrap'>
-                    <div className='text-sm text-gray-600'>17/06/2001</div></td>
+                    <div className='text-sm text-gray-600'>17/02/2024</div></td>
                     <td className='px-6 py-4 whitespace-nowrap'>
                     <div className='text-sm text-gray-600'>Software Devloper</div></td>
                     <td className='px-6 py-4 whitespace-nowrap'>
                     <div className='text-sm text-gray-600'>Online</div></td>
                     <td>
-                        <a href="#" className='text-indigo-600 hover:text-indigo-800 '>On Going</a>
+                        <a href="#" className='text-indigo-600 hover:text-indigo-800 '>On Project</a>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td className='px-6 py-4 whitespace-nowrap'>
+                        <div className='text-sm text-gray-600'>2</div></td>
+                    <td className='px-6 py-4 whitespace-nowrap'>
+                    <div className='text-sm text-gray-600'>Anbu</div></td>
+                    <td className='px-6 py-4 whitespace-nowrap'>
+                    <div className='text-sm text-gray-600'>1/01/20024</div></td>
+                    <td className='px-6 py-4 whitespace-nowrap'>
+                    <div className='text-sm text-gray-600'>Software Devloper</div></td>
+                    <td className='px-6 py-4 whitespace-nowrap'>
+                    <div className='text-sm text-gray-600'>On Project</div></td>
+                    <td>
+                        <a href="#" className='text-indigo-600 hover:text-indigo-800 '>On Project</a>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td className='px-6 py-4 whitespace-nowrap'>
+                        <div className='text-sm text-gray-600'>3</div></td>
+                    <td className='px-6 py-4 whitespace-nowrap'>
+                    <div className='text-sm text-gray-600'>Shiva</div></td>
+                    <td className='px-6 py-4 whitespace-nowrap'>
+                    <div className='text-sm text-gray-600'>3/02/20024</div></td>
+                    <td className='px-6 py-4 whitespace-nowrap'>
+                    <div className='text-sm text-gray-600'>FullStack Devloper</div></td>
+                    <td className='px-6 py-4 whitespace-nowrap'>
+                    <div className='text-sm text-gray-600'>Offline</div></td>
+                    <td>
+                        <a href="#" className='text-indigo-600 hover:text-indigo-800 '>On Bench</a>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td className='px-6 py-4 whitespace-nowrap'>
+                        <div className='text-sm text-gray-600'>4</div></td>
+                    <td className='px-6 py-4 whitespace-nowrap'>
+                    <div className='text-sm text-gray-600'>Saran</div></td>
+                    <td className='px-6 py-4 whitespace-nowrap'>
+                    <div className='text-sm text-gray-600'>11/01/2001</div></td>
+                    <td className='px-6 py-4 whitespace-nowrap'>
+                    <div className='text-sm text-gray-600'>Web Devloper</div></td>
+                    <td className='px-6 py-4 whitespace-nowrap'>
+                    <div className='text-sm text-gray-600'>Online</div></td>
+                    <td>
+                        <a href="#" className='text-indigo-600 hover:text-indigo-800 '>On Project</a>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td className='px-6 py-4 whitespace-nowrap'>
+                        <div className='text-sm text-gray-600'>5</div></td>
+                    <td className='px-6 py-4 whitespace-nowrap'>
+                    <div className='text-sm text-gray-600'>Soundarya</div></td>
+                    <td className='px-6 py-4 whitespace-nowrap'>
+                    <div className='text-sm text-gray-600'>5/01/2024</div></td>
+                    <td className='px-6 py-4 whitespace-nowrap'>
+                    <div className='text-sm text-gray-600'>Software Devloper</div></td>
+                    <td className='px-6 py-4 whitespace-nowrap'>
+                    <div className='text-sm text-gray-600'>Offline</div></td>
+                    <td>
+                        <a href="#" className='text-indigo-600 hover:text-indigo-800 '>On Project</a>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td className='px-6 py-4 whitespace-nowrap'>
+                        <div className='text-sm text-gray-600'>6</div></td>
+                    <td className='px-6 py-4 whitespace-nowrap'>
+                    <div className='text-sm text-gray-600'>Varuma</div></td>
+                    <td className='px-6 py-4 whitespace-nowrap'>
+                    <div className='text-sm text-gray-600'>5/03/2001</div></td>
+                    <td className='px-6 py-4 whitespace-nowrap'>
+                    <div className='text-sm text-gray-600'>Software Devloper</div></td>
+                    <td className='px-6 py-4 whitespace-nowrap'>
+                    <div className='text-sm text-gray-600'>Online</div></td>
+                    <td>
+                        <a href="#" className='text-indigo-600 hover:text-indigo-800 '>On Bench</a>
                     </td>
                 </tr>
             </tbody>
